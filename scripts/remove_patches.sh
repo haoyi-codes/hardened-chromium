7b701d2fcd42aab1b04c1ed8b8a5d17d6f5a534c
aryan <admin@reticentadmin.com>
2025-04-03 05:01:13 +0100

    feat: remove use-adler-v1.patch since it is already present

--- scripts/remove_patches.sh
+++ scripts/remove_patches.sh
@@ -6,7 +6,7 @@
 # Copyright (c) 2025 Aryan
 # SPDX-License-Identifier: BSD-3-Clause
 
-# Version: 1.1.0
+# Version: 1.2.0
 
 readonly vanadium_patches_path="../patches/vanadium/"
 readonly trivalent_patches_path="../patches/trivalent/"
@@ -15,6 +15,7 @@ readonly trivalent_patches_path="../patches/trivalent/"
 rm "${trivalent_patches_path}/default-disable-3d-apis.patch"
 rm "${trivalent_patches_path}/disable-extensions-by-default.patch"
 rm "${trivalent_patches_path}/build-hardening.patch"
+rm "${trivalent_patches_path}/use-adler-v1.patch"
 
 # Vanadium
 rm "${vanadium_patches_path}"/[0-9][0-9][0-9][0-9]"-Restriction-of-dynamic-code-execution-via-seccomp-bp.patch"
